1377d8815b50f87cc81bd87ac7839884cecdd408
tiptip123 <josephbaguio327@gmail.com>
2023-11-17 13:18:47 +0800

    added code in main class

--- src/BankAccount/Main.java
+++ src/BankAccount/Main.java
@@ -4,4 +4,17 @@ package BankAccount;
 
 public class Main {
     
+    public static void main(String[] args) {
+
+        bank j = new bank();
+        j.setName("Joseph Jay B Baguio");
+        j.setAge(22);
+        j.setAccNum(143734);
+        j.setBalance(1.3, 1);
+
+        System.out.println("CHINA BANK!" + "\n" + "Name:" + j.getName() + "\n" + "Age:" + j.getAge() + "\n" + "Account Number:" + j.getaccNum() + "\n" + "Balance:" + j.getBalance());
+
+    }
+    
+    
 }
