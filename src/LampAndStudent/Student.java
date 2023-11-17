3757c404a5d9bacfea3115f14c2e2de16e574674
tiptip123 <josephbaguio327@gmail.com>
2023-11-17 14:20:45 +0800

    Added code in student class

--- src/LampAndStudent/Student.java
+++ src/LampAndStudent/Student.java
@@ -1,7 +1,19 @@
-
 package LampAndStudent;
 
-
 public class Student {
+
+    String name;
+    int roll_num;
+
     
+}
+class ans{
+    public static void main(String[] args) {
+    Student student = new Student();
+    student.name="Josip";
+    student.roll_num= 143;
+        System.out.println("Hi! my name is "+student.name + "and my id is "+ student.roll_num);
+        
+    }
+
 }
