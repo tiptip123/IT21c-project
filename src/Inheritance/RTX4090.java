322365e4320fbd35c5a6392d798c89fdafa7ec45
tiptip123 <josephbaguio327@gmail.com>
2023-11-17 13:34:12 +0800

    Added code in RTX4090 class

--- src/Inheritance/RTX4090.java
+++ src/Inheritance/RTX4090.java
@@ -2,6 +2,15 @@
 package Inheritance;
 
 
-public class RTX4090 {
+public class RTX4090 extends VideoCard {
     
+     RTX4090(String videoCard) {
+        super(videoCard);
+
+    }
+
+    void broke() {
+        System.out.println(videoCard + " is broken");
+
+    }
 }
