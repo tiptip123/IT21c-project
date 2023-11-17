59867cdc947f274dd5c64573d1aa187906c7c711
tiptip123 <josephbaguio327@gmail.com>
2023-11-17 13:55:13 +0800

    Added code in class Encapsulation

--- src/Encapsulation/Encapsulation.java
+++ src/Encapsulation/Encapsulation.java
@@ -2,7 +2,17 @@
 package Encapsulation;
 
 
-public class Encapsulation {
+public class Encapsulation extends Area {
+
+    public Encapsulation(int length, int width) {
+        super(length, width);
+    }
  
+     public void getArea(){
+            int area = length+width;
+            System.out.println("Area:"+area);
+        }
+    }
     
-}
+    
+
