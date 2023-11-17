de2f3c6fb6ea81b2eabccc0eff63e5d7614ec3b5
tiptip123 <josephbaguio327@gmail.com>
2023-11-17 13:16:22 +0800

    added code in bank class

--- src/BankAccount/bank.java
+++ src/BankAccount/bank.java
@@ -4,4 +4,46 @@ package BankAccount;
 
 public class bank {
     
+    private String name;
+    private int age;
+    private int accNum;
+    private double balance;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public long getaccNum() {
+        return accNum;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setName(String n) {
+        this.name = n;
+
+    }
+
+    public void setAge(int a) {
+        this.age = a;
+
+    }
+
+    public void setAccNum(int acc) {
+        this.accNum = acc;
+
+    }
+
+    public void setBalance(double b, double c) {
+        this.balance = b + c;
+
+    }
+
 }
+
