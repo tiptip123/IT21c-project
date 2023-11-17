d5421c0e7e3ecb5eca2d4ab34f5e5c72d5ec48e6
tiptip123 <josephbaguio327@gmail.com>
2023-11-17 13:34:53 +0800

    Added code in Main class

--- src/Inheritance/Main.java
+++ src/Inheritance/Main.java
@@ -4,4 +4,12 @@ package Inheritance;
 
 public class Main {
     
+     public static void main(String[] args) {
+        RTX4090 myVideoCard = new RTX4090("BrandNewGPU");
+        
+        myVideoCard.broke();
+        myVideoCard.expensive();
+        myVideoCard.nice();
+    }
+    
 }
