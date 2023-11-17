2e2fe2e2852a4d80e7812957c2a00f35f524c0da
tiptip123 <josephbaguio327@gmail.com>
2023-11-17 13:33:24 +0800

    Added code in VideoCard class

--- src/Inheritance/VideoCard.java
+++ src/Inheritance/VideoCard.java
@@ -4,4 +4,18 @@ package Inheritance;
 
 public class VideoCard {
     
+      String videoCard;
+    
+    public VideoCard(String videoCard){
+        this.videoCard = videoCard;
+    }
+    
+    
+    void nice(){
+        System.out.println(videoCard +" is nice");
+    }
+    void expensive(){
+        System.out.println(videoCard +" is expensive");
+    }
+    
 }
