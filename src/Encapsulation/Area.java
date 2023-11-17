6dbb91ceb2ae6d93c36618aa96401384670a36ba
tiptip123 <josephbaguio327@gmail.com>
2023-11-17 13:53:38 +0800

    Added code in Area class

--- src/Encapsulation/Area.java
+++ src/Encapsulation/Area.java
@@ -4,4 +4,13 @@ package Encapsulation;
 
 public class Area {
     
+    int length;
+        int width;
+        
+        Area (int length,int width){
+            this.length = length;
+            this.width = width;
+            
+    }
+    
 }
