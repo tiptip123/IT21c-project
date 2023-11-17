b37b82b3326554e034bb3214b4853fb5523ce77c
tiptip123 <josephbaguio327@gmail.com>
2023-11-17 13:26:02 +0800

    added code in class main

--- src/Student/Main.java
+++ src/Student/Main.java
@@ -4,4 +4,19 @@ package Student;
 
 public class Main {
     
+     public static void main(String [] args){
+    
+   Student student = new Student();
+    student.setName("joseph Jay Baguio");
+     student.setSection("it21C");
+     student.setMotto("susuka peru di susuko");
+    
+    
+    
+        System.out.println("Name: "+ student.name);
+        System.out.println("Section: "+ student.section);
+        System.out.println("Motto: "+ student.motto);
+    
+    }    
+    
 }
