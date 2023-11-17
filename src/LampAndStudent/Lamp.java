3038673aecdce9f5ad02bdf3e85a9bcf47d134f5
tiptip123 <josephbaguio327@gmail.com>
2023-11-17 14:24:48 +0800

    Added code in lamp class

--- src/LampAndStudent/Lamp.java
+++ src/LampAndStudent/Lamp.java
@@ -1,7 +1,27 @@
-
 package LampAndStudent;
 
-
 public class Lamp {
+
+    boolean is_on;
+
+    void turnOn() {
+        System.out.println("light's on!");
+    }
+
+    void turnOff() {
+        System.out.println("light's off!");
+
+    }
+}
+class main{
     
+public static void main (String [] args){
+    Lamp led = new Lamp();
+    Lamp Halogen = new Lamp();
+    led.turnOff();
+    Halogen.turnOn();
+    
+}
+
+
 }
