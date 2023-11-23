68c037a3001c102e0cd4bd71fd076686a474cff8
tiptip123 <josephabguio327@gmail.com>
2023-11-23 16:10:16 +0800

    added method in mother class

--- src/Polymorphism/Main.java
+++ src/Polymorphism/Main.java
@@ -5,7 +5,13 @@ package Polymorphism;
 public class Main {
     
     public static void main(String [ ]args){
+        Runner runner = new Runner("runners");
         
+        
+        
+        
+        
+        runner.RunnerPlacement();
         
     }
     
--- src/Polymorphism/Runner.java
+++ src/Polymorphism/Runner.java
@@ -3,5 +3,12 @@ package Polymorphism;
 
 
 public class Runner {
+    String name;
     
+    Runner(String name){
+        this.name = name;
+    }
+    public void RunnerPlacement(){
+        System.out.println( name +" are running");
+}
 }
