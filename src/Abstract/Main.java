6f9bdde20071bddfe3f09134ddbbaf245726d2d4
tiptip123 <josephbaguio327@gmail.com>
2023-11-23 22:56:45 +0800

    added code in main class and this is my final commit

---
+++ src/Abstract/Main.java
@@ -0,0 +1,17 @@
+package Abstract;
+
+public class Main {
+
+    public static void main(String[] args) {
+        josip swim = new josip("Josip");
+        swim.doing();
+        swim.SwimmerName();
+
+        lester les = new lester("Lester");
+        les.SwimmerName();
+
+        mj m = new mj("Mj");
+        m.SwimmerName();
+    }
+
+}
