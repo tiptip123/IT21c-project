e123cdf7133fc13f0751df4bd8e73f13d75df175
tiptip123 <josephbaguio327@gmail.com>
2023-11-23 22:55:44 +0800

    added code in subclass lester

---
+++ src/Abstract/lester.java
@@ -0,0 +1,14 @@
+package Abstract;
+
+
+public class lester  extends Swimmer{
+    String name;
+
+    public lester(String name) {     
+        this.name = name;
+    }
+    
+    public  void SwimmerName(){
+        System.out.println(name +" is following josip");
+    }
+}
