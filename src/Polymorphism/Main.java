0c21db5d3fb5c781068a3c27445d69833e5d720a
tiptip123 <josephabguio327@gmail.com>
2023-11-23 16:28:12 +0800

    added child class runnerMj and added some code

--- src/Polymorphism/Main.java
+++ src/Polymorphism/Main.java
@@ -10,12 +10,15 @@ public class Main {
         runnerLester lester = new runnerLester("lester");
         runnerJoash joash = new runnerJoash("Joash");
         runnerJethro jethro = new runnerJethro("Jethro");
+        runnerMj mj = new runnerMj("Mj");
+        
         
         runner.RunnerPlacement();
         josip.RunnerPlacement();
         lester.RunnerPlacement();
         joash.RunnerPlacement();
         jethro.RunnerPlacement();
+        mj.RunnerPlacement();
     }
     
 }
---
+++ src/Polymorphism/runnerMj.java
@@ -0,0 +1,14 @@
+
+package Polymorphism;
+
+
+public class runnerMj {
+    String name;
+    
+    runnerMj(String name){
+     this.name = name;   
+    }
+    public void RunnerPlacement(){
+        System.out.println( name +" last placer kay naka igit");
+}
+}
