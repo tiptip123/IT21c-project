2ef1bd968964412e038622bafcb0cf56707c6143
tiptip123 <josephabguio327@gmail.com>
2023-11-24 16:47:46 +0800

    added subclass Cat

---
+++ src/Abstract_Color_LAB_Exrecise/Cat.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Abstract_Color_LAB_Exrecise;
+
+/**
+ *
+ * @author Admin
+ */
+public class Cat {
+    
+}
