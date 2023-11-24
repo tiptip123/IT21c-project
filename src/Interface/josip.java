76343dc74aab4179d1437b7b4ffd876909302e35
tiptip123 <josephabguio327@gmail.com>
2023-11-24 16:16:01 +0800

    added code in subclass josip

--- src/Interface/josip.java
+++ src/Interface/josip.java
@@ -2,6 +2,19 @@
 package Interface;
 
 
-public class josip {
-    
+ class josip implements Hackers{
+     String name;
+     
+     josip(String name){
+      this.name = name;   
+     }
+     
+    public void did(){
+        System.out.println(name +" hacked nasa last month.");
+        
+    }
+    public void didNot() {
+        System.out.println(name + "did not hack gcash kay taas ug security");
+        
+    }
 }
